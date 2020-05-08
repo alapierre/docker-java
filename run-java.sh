cf8f35d4f0a9bdc2900ba45603a02c0a70210af0
Adrian Lapierre <adrian.lapierre@sidgroup.pl>
2020-05-08 15:43:16 +0200

    fix in run_java.sh to avoid error message from sh

--- run-java.sh
+++ run-java.sh
@@ -10,10 +10,10 @@
 
 [ -z "$JAVA_OPTS" ] && echo "JAVA_OPTS not set"
 
-if [ -z $JAVA_OPTS]; then                                                                                                                                                                        
+if [ -z $JAVA_OPTS ]; then
   echo "running java with NO JAVA_OPTS  as:" exec java -Djava.security.egd=file:/dev/./urandom -jar "$@"
-  exec java -Djava.security.egd=file:/dev/./urandom -jar "$@"                                   
-else                                                                                              
+  exec java -Djava.security.egd=file:/dev/./urandom -jar "$@"
+else
   echo "running java with JAVA_OPTS as:" exec java "$JAVA_OPTS" -Djava.security.egd=file:/dev/./urandom -jar "$@"
   exec java "$JAVA_OPTS" -Djava.security.egd=file:/dev/./urandom -jar "$@"
 fi
